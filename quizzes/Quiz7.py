f377f1b9212f7e5dc80b5a763b0cc6300106f30b
Glenn Downing <downing@cs.utexas.edu>
2012-09-19 16:21:28 -0500

    another commit

---
+++ quizzes/Quiz7.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+
+"""
+CS373: Quiz #7 (5 pts)
+"""
+
+""" ----------------------------------------------------------------------
+1. What are the two most common problems with releasing changes?
+   [XP: Ch. 15, Pg. 124-125]
+   (2 pts)
+
+slow merges
+lost changes
+"""
+
+""" ----------------------------------------------------------------------
+2. What are the two classical ways to implement a graph?
+   (1 pt)
+
+adjacency matrix and adjacency list
+"""
+
+""" ----------------------------------------------------------------------
+3. What graph property leads to one vs. the other of those two
+   implementations?
+   (1 pt)
+
+sparse vs. dense
+"""
